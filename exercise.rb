5baaf5bca2e339be5055e0e1bb7f6d34b682a3a7
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-09 12:23:17 -0500

    add japanese and readme information

--- exercise.rb
+++ exercise.rb
@@ -3,11 +3,12 @@
 digits = ['1','2','3','4','5','6','7','8','9']
 en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+jp = ['一','二','三',' ','五','六','七','八','九']
 
 my_hash = {}
 
 digits.each {|num|
-  my_hash[num] = {french: fr[num.to_i - 1], english: en[num.to_i - 1]}
+  my_hash[num] = {french: fr[num.to_i - 1], english: en[num.to_i - 1], japanese: jp[num.to_i - 1]}
 }
 
 # ap my_hash
